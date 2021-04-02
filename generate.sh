1755ec96cc4b96a5246659209e487e15c5b76f37
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2021-04-02 19:07:00 +0300

    Pass the right thing [skip ci]

--- generate.sh
+++ generate.sh
@@ -25,17 +25,18 @@ fi
 for ymlConfig in ./resty/*.yml
 do
   echo "${bold}====== Processing $ymlConfig ======${normal}"
-  moduleName="lua-resty-$(basename $ymlConfig .yml)"
+  LUA_PKG_NAME="resty-$(basename $ymlConfig .yml)"
+  moduleName="lua-${LUA_PKG_NAME}"
   printf "Generating ${moduleName}.spec ...  "
   # generate .spec by feeding module .yml to lastversion, then template result via jinja2cli
   lastversion ${ymlConfig} --format json  |
-    jinja2 -D name=${moduleName} --format=json spec.j2 --strict - > ${moduleName}.spec
+    jinja2 -D luapkgname=${LUA_PKG_NAME} --format=json spec.j2 --strict - > ${moduleName}.spec
   echo "Done!"
 
   printf "Checking with rpmlint ...:  "
   # rpmlint -f ./rpmlint.config ./${moduleName}.spec
 done
-
+exit
 git add --all .
 # at this point we don't know what updated: rpmmacros or spec files, but we check-in all
 git commit -m "Updated"
