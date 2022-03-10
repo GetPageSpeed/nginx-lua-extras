79d7f0477ff4a1627264b668788755c48c39890c
Danila Vershinin <ciapnz@gmail.com>
2022-03-10 10:50:34 +0300

    up [skip ci]

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 set -x
 # ensure github api token added
-source ~/.bashrc
+source "${HOME}/.bashrc"
 cd "$(dirname "$0")"
 
 git fetch --all
@@ -22,20 +22,24 @@ else
     normal=""
 fi
 
-for ymlConfig in ./resty/*.yml
-do
+for ymlConfig in ./resty/*.yml; do
   echo "${bold}====== Processing $ymlConfig ======${normal}"
   LUA_PKG_NAME="resty-$(basename $ymlConfig .yml)"
   moduleName="lua-${LUA_PKG_NAME}"
-  printf "Generating ${moduleName}.spec ...  "
+  echo "Generating ${moduleName}.spec ...  "
   # generate .spec by feeding module .yml to lastversion, then template result via jinja2cli
-  lastversion ${ymlConfig} --format json  |
+  lastversion "${ymlConfig}" --format json  |
     jinja2 -D luapkgname=${LUA_PKG_NAME} --format=json spec.j2 --strict - > ${moduleName}.spec
   echo "Done!"
 
   printf "Checking with rpmlint ...:  "
   # rpmlint -f ./rpmlint.config ./${moduleName}.spec
 done
+
+# remove problematic packages:
+# lua-resty-newrelic.src: W: invalid-url Source0: https://github.com/saks/lua-resty-newrelic/archive/v0.01-6/lua-resty-newrelic-v0.01-6.tar.gz HTTP Error 300: Multiple Choices
+rm -rf ./lua-resty-newrelic.spec
+
 git add --all .
 # at this point we don't know what updated: rpmmacros or spec files, but we check-in all
 git commit -m "Updated"
